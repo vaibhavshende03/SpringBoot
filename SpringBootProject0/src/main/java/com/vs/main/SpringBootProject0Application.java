3c3719a5bc621a0dedf24d0d0089b073dc2b06dd
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2025-02-21 14:35:44 +0530

    Standalone Application

---
+++ SpringBootProject0/src/main/java/com/vs/main/SpringBootProject0Application.java
@@ -0,0 +1,13 @@
+package com.vs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject0Application.class, args);
+	}
+
+}
