bec208eac40748e336db9b2f5d0596887308cdb0
VAIBHAV SHENDE <97295088+vaibhavshende03@users.noreply.github.com>
2025-02-28 00:12:56 +0530

    Delete SpringBootProject06SpringBootWebApp1 directory

--- SpringBootProject06SpringBootWebApp1/src/main/java/com/vs/main/services/UserServices.java
+++
@@ -1,9 +0,0 @@
-package com.vs.main.services;
-
-import com.vs.main.entities.User;
-
-public interface UserServices {
-	
-	public boolean regiteruser(User user);
-	public User loginuser(String email,String password);
-}
