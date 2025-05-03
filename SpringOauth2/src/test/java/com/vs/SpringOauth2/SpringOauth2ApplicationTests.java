900f31e503567c0d107621356b99fcfdb80fc006
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2025-05-04 00:16:53 +0530

    Implement the oauth2Login

---
+++ SpringOauth2/src/test/java/com/vs/SpringOauth2/SpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.SpringOauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
